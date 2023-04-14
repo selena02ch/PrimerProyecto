609c767ca81678c8821bec6cacf2e4be2485b6c9
Selena Chaparro <selewilk95@gmail.com>
2023-04-14 18:51:24 -0300

    primerproyecto

---
+++ src/udiclase/UDIclase.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package udiclase;
+import java.util.*;
+/**
+ *
+ * @author SUPCOMERCIO
+ */
+public class UDIclase {
+private Scanner teclado;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner teclado = new Scanner(System.in);
+        String nombre;
+        System.out.println("Ingrese nombre y apellido");
+        nombre = teclado.next();
+    }
+    
+}
